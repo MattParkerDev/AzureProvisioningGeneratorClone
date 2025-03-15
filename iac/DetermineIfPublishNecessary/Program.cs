af741272d2b90335e3109e63692c45f6d7c5ce7a
Matthew Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2025-03-15 12:44:51 +1000

    asdf

---
+++ iac/DetermineIfPublishNecessary/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DetermineIfPublishNecessary;
+
+var gitRootDirectory = GitRoot.GetGitRootPath();
+var packageProjectFilePath = Path.Combine(gitRootDirectory, "src", "Meta.Azure.Provisioning", "Meta.Azure.Provisioning.csproj");
