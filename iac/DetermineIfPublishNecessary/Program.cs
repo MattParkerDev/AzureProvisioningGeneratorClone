f9df6af0044259e5b21ff9b10c8957ec2ba7baf4
Matthew Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2025-03-15 13:06:37 +1000

    more

--- iac/DetermineIfPublishNecessary/Program.cs
+++ iac/DetermineIfPublishNecessary/Program.cs
@@ -1,6 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DetermineIfPublishNecessary;
+using Microsoft.Build.Definition;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Locator;
 
-using DetermineIfPublishNecessary;
+MSBuildLocator.RegisterDefaults();
+Run(); // https://learn.microsoft.com/en-us/visualstudio/msbuild/find-and-use-msbuild-versions?view=vs-2022#register-instance-before-calling-msbuild
 
-var gitRootDirectory = GitRoot.GetGitRootPath();
-var packageProjectFilePath = Path.Combine(gitRootDirectory, "src", "Meta.Azure.Provisioning", "Meta.Azure.Provisioning.csproj");
+return;
+
+void Run()
+{
+	var gitRootDirectory = GitRoot.GetGitRootPath();
+	var packageProjectFilePath = Path.Combine(gitRootDirectory, "src", "Meta.Azure.Provisioning", "Meta.Azure.Provisioning.csproj");
+
+	var fileInfo = new FileInfo(packageProjectFilePath);
+	if (fileInfo.Exists is false)
+	{
+		throw new FileNotFoundException(packageProjectFilePath);
+	}
+
+	Console.WriteLine($"Found project file at: {packageProjectFilePath}");
+
+	var project = Project.FromFile(packageProjectFilePath, new ProjectOptions());
+	;
+}
