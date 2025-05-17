ccdb1e6e7fd265d30628e8641465a3cce427f75d
Matt Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2025-05-17 14:53:09 +1000

    Copy v1

--- tools/SourceSync/Program.cs
+++ tools/SourceSync/Program.cs
@@ -1,2 +1,15 @@
-﻿
+﻿using SourceSync;
+
 Console.WriteLine("Starting Source Sync...");
+
+// provide a link to the sdk/provisioning folder
+var sdkProvisioningPath = @"C:\Users\Matthew\Documents\Git\azure-sdk-for-net\sdk\provisioning";
+var destinationPath = @"C:\Users\Matthew\Documents\Git\AzureProvisioningGeneratorClone\src\Meta.Azure.Provisioning\Generated";
+
+// get folder names starting with Azure.
+var folderNames2 = Directory.EnumerateDirectories(sdkProvisioningPath, "TestFolder*", SearchOption.TopDirectoryOnly)
+	//.Select(Path.GetFileName)
+	.ToList();
+
+;
+Copy.CopyDirectory(folderNames2[0], destinationPath, true);
