b6c3892ec24e4bc9e6bb2176016bcb07b4ddc7b0
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-06-12 23:45:54 +1000

    Add generated code cleaner

---
+++ tools/ClearGeneratedCode/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var rootPath = @"C:\Users\Matthew\Documents\Git\AzureProvisioningGeneratorClone\src\Meta.Azure.Provisioning\Generated";
+
+var directories = Directory.EnumerateDirectories(rootPath, "*", SearchOption.TopDirectoryOnly).ToList();
+
+var directoriesToRemove = directories.Select(s => Path.Combine(s, "src", "Generated")).ToList();
+
+foreach (var directory in directoriesToRemove)
+{
+	if (Directory.Exists(directory))
+	{
+		Console.WriteLine($"Removing directory: {directory}");
+		//Directory.Delete(directory, true);
+	}
+	else
+	{
+		Console.WriteLine($"Directory does not exist: {directory}");
+	}
+}
